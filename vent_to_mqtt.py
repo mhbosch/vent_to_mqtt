872f98ff1db3fdfdbd8783cbad878dd772f1c79a
mhbosch <poststelle@michael-h-bosch.de>
2020-05-20 09:25:57 +0200

    Update vent_to_mqtt.py
    Version 0.5. not stable by reconnect

--- vent_to_mqtt.py
+++ vent_to_mqtt.py
@@ -1,14 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import paho.mqtt.client as mqttClient
-from ecovent.host import Fan
-#import paho.mqtt.publish as mqttPublish
 import os
 import datetime
 import time
 import socket
 import signal
 import sys
+
+
+'''
+        Dieses Programm ermöglicht es, einen Blauberg Ventilator per mqtt in eine Hausautomationsumgebung einzubinden.
+        
+        Hauptprogramm: Michael H. Bosch
+        Routinen parsebytes und parse_response sind dem Modul pyEcovent entnommen, welches unter MIT Lizenz veröffentlicht wurde: https://github.com/aglehmann/pyEcovent
+
+
+'''
+
+
+class Ventilator:
+    def __init__(self, name):
+       self.name = name
+
+    def parsebytes(self, bytestring, params):
+        i = iter(bytestring)
+        for param in i:
+            value = [next(i) for _ in range(params[param][0])]
+            yield(param,value)
+
+    def parse_response(self, data):
+        fan_params = {
+        0x03: [1, 'state', None],
+        0x04: [1, 'speed', None],
+        0x05: [1, 'manual_speed', None],
+        0x06: [1, 'air_flow_direction', None],
+        0x08: [1, 'humidity_level', None],
+        0x09: [1, 'operation_mode', None],
+        0x0B: [1, 'humidity_sensor_threshold', None],
+        0x0C: [1, 'alarm_status', None],
+        0x0D: [1, 'relay_sensor_status', None],
+        0x0E: [3, 'party_or_night_mode_countdown', None],
+        0x0F: [3, 'night_mode_timer', None],
+        0x10: [3, 'party_mode_timer', None],
+        0x11: [3, 'deactivation_timer', None],
+        0x12: [1, 'filter_eol_timer', None],
+        0x13: [1, 'humidity_sensor_status', None],
+        0x14: [1, 'boost_mode', None],
+        0x15: [1, 'humidity_sensor', None],
+        0x16: [1, 'relay_sensor', None],
+        0x17: [1, '10V_sensor', None],
+        0x19: [1, '10V_sensor_threshold', None],
+        0x1A: [1, '10V_sensor_status', None],
+        0x1B: [32, 'slave_device_search', None],
+        0x1C: [4, 'response_slave_search', None],
+        0x1F: [1, 'cloud_activation', None],
+        0x25: [1, '10V_sensor_current_status', None]
+        }
+
+        for pair in self.parsebytes(data, fan_params):
+            if pair[0] == 3:
+                self.state = pair[1][0]
+            elif pair[0] == 4:
+                self.speed = pair[1][0]
+            elif pair[0] == 5:
+                self.man_speed = pair[1][0]
+            elif pair[0] == 6:
+                self.airflow = pair[1][0]
+            elif pair[0] == 8:
+                self.humidity = pair[1][0]
 
 
 def on_connect(client, userdata, flags, rc):
@@ -32,22 +92,25 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    global Ventilator_aus
-    global Ventilator_an
-    print("Messagetopc="+message.topic + " Message=" + str(message.payload))
+    global Ventilator_state
+    global Ventilator_speed
+    global Ventilator_airflow
+
+    print("Messagetopic=" + message.topic + " Message=" + str(message.payload))
     data = str(message.payload)
     data = data[2:len(data)-1]
     print("Message="+data)
 
-    if str(message.topic) == "Vent/Blauberg/Command":
-        if data == 'ON':
-            print("Ventilator anschalten!")
-            Ventilator_aus = False
-            Ventilator_an = True
-        if data == 'OFF':
-            print("Ventilator ausschalten!")
-            Ventilator_an = False
-            Ventilator_aus = True
+    if str(message.topic) == "Vent/Blauberg/Command/State":
+        Ventilator_state = data
+
+    if str(message.topic) == "Vent/Blauberg/Command/Speed":
+        Ventilator_speed = data
+
+    if str(message.topic) == "Vent/Blauberg/Command/Airflow":
+        Ventilator_airflow = data
+
+
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print('Subscribe empfangen: ' + str(mid))
@@ -63,13 +126,9 @@ def on_disconnect(client, userdata, rc):
         client.reconnect()
 
 
-def send_mqtt(fan):
-    client.publish("Vent/Blauberg/State", str(fan.state))
-    client.publish("Vent/Blauberg/Fan_Speed", fan.speed)
-    client.publish("Vent/Blauberg/man_Speed", fan.man_speed)
-    client.publish("Vent/Blauberg/Humidity", fan.humidity)
-    client.publish("Vent/Blauberg/Airflow", fan.airflow)
-    client.publish("Vent/Blauberg/Update", "Online")
+def send_mqtt(msgs):
+    client.publish("Vent/Blauberg/Status", str(msgs))
+    client.publish("Vent/Blauberg/Service", "Online")
 
 
 def connect_start(client):
@@ -88,12 +147,18 @@ def connect_start(client):
 def abfragenloop():
     global socket_fehler
     global sleeptime
-    global Ventilator_aus
-    global Ventilator_an
-    wait_vent = 0
+    global Ventilator_state
+    global Ventilator_speed
+    global Ventilator_airflow
+    global Ventilator_IP
+    wait_vent = 1
     alterstatus = ""
+
+    HEADER = bytes.fromhex('6D6F62696C65')
+    FOOTER = bytes.fromhex('0D0A')
+
     # Voreinstellungen für Host und Port
-    host = str(vent.host)
+    host = str(Ventilator_IP)
     port = 4000
     # Die zu sendenden Daten
     data = bytes.fromhex('6D6F62696C6501000D0A')
@@ -111,33 +176,43 @@ def abfragenloop():
             now = datetime.datetime.now()
             print(now.strftime('%d.%m.%Y - %H:%M:%S') +
                   " Antwort=" + str(response))
-            vent.parse_response(response[6:])
-            status = str(vent.state) + str(vent.speed) + str(vent.man_speed) + str(vent.humidity) + str(vent.airflow)
-            if (Ventilator_aus == True) and (vent.state == "on"):
-                print("Vent aus:")
+            Vent.parse_response(response[6:])
+            status = str(Vent.state) + "-" + str(Vent.speed) + "-"  + str(Vent.man_speed) + "-" + str(Vent.humidity) + "-"  + str(Vent.airflow)
+            if Ventilator_state == False:
+                Ventilator_state = Vent.state
+            if Ventilator_speed == False:
+                Ventilator_speed = Vent.speed
+            if Ventilator_airflow == False:
+                Ventilator_airflow = Vent.airflow
+            
+            #print ("Vent speed=" + str(Vent.speed) + str(Ventilator_speed))
+            if int(Ventilator_state) != int(Vent.state):
+                print ("Der An/Aus Status ist verschieden! " + str(Ventilator_state ) + " - " + str(Vent.state) )
                 UDPSock.sendto(bytes.fromhex('6D6F62696C6503000D0A'), addr)
-            if (Ventilator_aus == True) and (vent.state == "off"):
-                    Ventilator_aus = False
-                    print("Die Variable Ventilator_aus wird auf False gesetzt")
-            if (Ventilator_an == True) and (vent.state == "off"):
-                print("Vent an:")
-                UDPSock.sendto(bytes.fromhex('6D6F62696C6503000D0A'), addr)
-            if (Ventilator_an == True) and (vent.state == "on"):
-                    Ventilator_an = False
-                    print("Die Variable Ventilator_an wird auf False gesetzt")
-            print(status + " Anzahl der gesamten Socket_Error = " + str(socket_fehler))
+            if int(Ventilator_speed) != int(Vent.speed):
+                print ("Der Geschwindigkeits Status ist verschieden! " + str(Ventilator_speed ) + " - " + str(Vent.speed) )
+                cmd= '04' + '{0:0{1}x}'.format(int(Ventilator_speed),2)
+                UDPSock.sendto((HEADER + bytes.fromhex(cmd) + FOOTER), addr)
+            if int(Ventilator_airflow) != int(Vent.airflow):
+                print ("Der Airflow Status ist verschieden! " + str(Ventilator_airflow ) + " - " + str(Vent.airflow) )
+                cmd= '06' + '{0:0{1}x}'.format(int(Ventilator_airflow),2)
+                UDPSock.sendto((HEADER + bytes.fromhex(cmd) + FOOTER), addr)
+                
+            #print("Status = " + status) # + " Anzahl der gesamten Socket_Error = " + str(socket_fehler))
+            payload = { 'State': str(Vent.state),'Humidity': str(Vent.humidity), 'Speed': str(Vent.speed), 'Airflow': str(Vent.airflow), 'Man_speed' : str(Vent.man_speed)}
+            print (payload)
             if alterstatus != status:
-                send_mqtt(vent)
+                send_mqtt(payload)
             alterstatus = status
-            client.publish("Vent/Blauberg/Status", "Online")
-            wait_vent = 0
+            client.publish("Vent/Blauberg/Service", "Online")
+            wait_vent = 1
             signal.signal(signal.SIGTERM, exit_gracefully)
             time.sleep(sleeptime)
         except socket.timeout as error:
             print("Socket Error: %s" % error)
-            client.publish("Vent/Blauberg/Status", "Offline")
+            client.publish("Vent/Blauberg/Service", "TimeOut")
             socket_fehler += 1
-            time.sleep(sleeptime )
+            time.sleep(sleeptime)
             now = datetime.datetime.now()
             print(now.strftime('%d.%m.%Y - %H:%M:%S') +
                   " - Warte auf eine Antwort vom Ventilationssystem #" + str(wait_vent))
@@ -157,7 +232,7 @@ def abfragenloop():
 
 
 def exit_gracefully(signum, frame):
-    client.publish("Vent/Blauberg/Status", "Offline")
+    client.publish("Vent/Blauberg/Service", "Service Down")
     print("Beende den Dienst aufgrund TERM Signal")
     client.disconnect()
     time.sleep(1)
@@ -165,19 +240,21 @@ def exit_gracefully(signum, frame):
 
 # ---------------------------------------------------------------------
 Connected = False
-fan = Fan("192.168.x.xxx")  # Adresse des Ventilators
-vent = fan  # Adresse des Ventilators, nur zum parsen des Sockets ohne Close des Sockets
-anzahl_fehler = 0  # Initialisierung
-max_retries = 10  # Anzahl der Timeouts, bevor die Verbindung als OFFLINE gemeldet wird
-sleeptime = 5  # Sekunden zwischen den Abfragen
-socket_fehler = 0
-Ventilator_an = False
-Ventilator_aus= False
+Vent = Ventilator("Bosch")
+Ventilator_IP = "IP_OF_VENT"
+anzahl_fehler   = 0  # Initialisierung
+#max_retries     = 10  # Anzahl der Timeouts, bevor die Verbindung als OFFLINE gemeldet wird
+sleeptime       = 5  # Sekunden zwischen den Abfragen
+socket_fehler   = 0
+Ventilator_state = False
+Ventilator_speed = False
+Ventilator_airflow = False
 
-broker_address = "127.0.0.1"  # Your MQTT broker IP address
+
+broker_address = "BROKER_IP"  # Your MQTT broker IP address
 port = 1883  # default port change as required
-user = "dein_user"  # mqtt user name change as required
-password = "dein_passwort"  # mqtt password change as required
+user = "_USER_"  # mqtt user name change as required
+password = "_PASSWORD_"  # mqtt password change as required
 client = mqttClient.Client("Vent")
 client.username_pw_set(user, password=password)
 client.on_connect = on_connect
@@ -189,8 +266,10 @@ client.on_disconnect = on_disconnect
 connect_start(client)
 
 # lauschen ob Befehle gesendet werden
-client.subscribe("Vent/Blauberg/Command", 0)
+client.subscribe([("Vent/Blauberg/Command/State", 0),("Vent/Blauberg/Command/Speed", 0),("Vent/Blauberg/Command/Airflow", 0)])
 client.loop_start()
+
+
 
 while Connected != True:
     time.sleep(2)
@@ -204,8 +283,9 @@ try:
         signal.signal(signal.SIGTERM, exit_gracefully)
         abfragenloop()
 
+
 except (KeyboardInterrupt, SystemExit):
-    client.publish("Vent/Blauberg/Status", "Offline")
+    client.publish("Vent/Blauberg/Service", "Service Down")
     print("Starte Cleanup")
     client.loop_stop()
     time.sleep(2)
